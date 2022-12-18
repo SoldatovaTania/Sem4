1d4ea23782bb63fb1efc8fa47fc0074872df2aae
SoldatovaTania <tatyana.soldatova.96@mail.ru>
2022-12-18 19:36:12 +0300

    Д/З
    Лекция 4

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+Ограничения:
+- Нельзя использовать класс Math
+- Должна быть отдельная функция Power, которая возвразает результат
+- Внутри функций НЕЛЬЗЯ использовать класс Console. Только в основном блоке кода*/
+
+
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
---
+++ task3/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 29: Напишите программу, которая принимает на вход числа n, min, max. Затем генерирует массив размером n и заполняет его случайными числами в диапазоне от min до max.
+//5, 1, 19 -> [1, 2, 5, 7, 19]
+//3, 1, 33 -> [6, 1, 33]
+
+int lenArray = ReadInt("Введите длинну массива: ");
+int Min = ReadInt("Введите минимальное число: ");
+int Max = ReadInt("Введите максимальное число: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(Min,Max);
+    Console.Write(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
